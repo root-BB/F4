7fba037f2da753d8460063addab248f014ef54e4
BB <28000223+root-BB@users.noreply.github.com>
2021-08-10 01:11:11 +0300

    Script Examples 1.0.1.0 Upload
    Script Examples Uploaded

---
+++ F4 - File.ps1
@@ -0,0 +1,14 @@
+﻿# Created By BaHTsIzBEdEvi
+$Target = "C:\"
+$MinSize = 0.2
+$Table = @()
+gci -Force -Recurse $Target -ErrorAction SilentlyContinue | % {
+$Path = $_.fullname
+$Size = $_.length / 1Gb
+if ($Size -gt $MinSize) {
+$Folder = New-Object PSObject
+Add-Member -inputObject $Folder -memberType NoteProperty -name “Path” -value $Path
+Add-Member -inputObject $Folder -memberType NoteProperty -name “Size” -value $Size
+$Table += $Folder }
+}
+$Table | Out-GridView -Title “Files Over $MinSize Gb”
---
+++ F4 - Folder.ps1
@@ -0,0 +1,15 @@
+﻿# Created By BaHTsIzBEdEvi
+$Target = "C:\"
+$MinSize = 3.0
+$Table = @()
+gci -Force -Recurse $Target -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+$Fsize = 0
+gci -Force -Recurse $_.fullname -ErrorAction SilentlyContinue | % { $Fsize += $_.length }
+$Path = $_.fullname
+$Size = $Fsize / 1Gb
+if ($Size -gt $MinSize) {
+$Folder = New-Object PSObject
+Add-Member -inputObject $Folder -memberType NoteProperty -name “Path” -value $Path
+Add-Member -inputObject $Folder -memberType NoteProperty -name “Size” -value $Size
+$Table += $Folder }}
+$Table | Out-GridView -Title “Folders Over $MinSize Gb”
